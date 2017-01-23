c906c2535a55f7f7cd9834273f269d9fa1437131
Abshir Ahmed <abshir.ahmed@outlook.com>
2017-01-23 21:20:31 +0000

    changed layout and added a few extras

--- app/src/main/java/group1/simplebloodbank/MapsActivity.java
+++ app/src/main/java/group1/simplebloodbank/MapsActivity.java
@@ -1,26 +1,52 @@
 package group1.simplebloodbank;
 
+import android.*;
+import android.Manifest;
+import android.app.Dialog;
+import android.content.pm.PackageManager;
+import android.location.Geocoder;
+import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.identity.intents.Address;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.io.IOException;
 import java.util.List;
 
 import group1.simplebloodbank.model.BloodBank;
 import group1.simplebloodbank.network.BloodBankNetworkListener;
 import group1.simplebloodbank.network.NetworkManager;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback , BloodBankNetworkListener{
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, BloodBankNetworkListener {
 
     private GoogleMap mMap;
     private List<BloodBank> mBloodBanks;
     private NetworkManager mNetworkManager;
+    private float latitude;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
 
     @Override
@@ -28,13 +54,37 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
 
-        mNetworkManager =  new NetworkManager();
+        if (googleServicesAvailable()) {
+            Toast.makeText(this, "Perfect", Toast.LENGTH_SHORT).show(); //debug
+
+        }
+
+        mNetworkManager = new NetworkManager();
         mNetworkManager.setListener(this);
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
+
+    public boolean googleServicesAvailable() {//checks if googleServices is running on device
+        GoogleApiAvailability api = GoogleApiAvailability.getInstance();
+        int isAvailable = api.isGooglePlayServicesAvailable(this);
+        if (isAvailable == ConnectionResult.SUCCESS) {
+            Toast.makeText(this, "play services running fine", Toast.LENGTH_SHORT).show();
+
+            return true;
+        } else if (api.isUserResolvableError(isAvailable)) {
+            Dialog dialog = api.getErrorDialog(this, isAvailable, 0);
+            dialog.show();
+        } else {
+            Toast.makeText(this, "Cannot connect to play services", Toast.LENGTH_SHORT).show();
+        }
+        return false;
     }
 
 
@@ -58,18 +108,126 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onRequestCompleted(List<BloodBank> bloodBanks) {
 
         for (BloodBank bloodBank : bloodBanks) {
+
             setMapMarkers(bloodBank.getName(), bloodBank.getLatitude(), bloodBank.getLongitude());
+
         }
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED ||
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+
+            mMap.setMyLocationEnabled(true);
+
+        }
+
+
+        //clear markers after new loaction is entered
+
+        //create listener that will catch user attempt on
+        //clicking location and send name variable to our
+        //local server to update database
+
+        /*
+
+        if (user clicks tag){
+
+        //send corresponding postcode or hospital name string to server.
+
+        dataToClient.writeObject("Registered");
+		dataToClient.flush();
+
+        }
+
+        */
+
 
     }
 
-    private void setMapMarkers(String name, float latitude, float longitude) {
+    private void setMapMarkers(String name, double latitude, double longitude) {
+
+        float zoom = (float) 7.5;
         mMap.addMarker(new MarkerOptions().position(new LatLng(latitude, longitude)).title(name));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(latitude,longitude)));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(latitude, longitude), zoom));
+
+        //Jie this is where you can add what ever you want when somebody clicks on the marker
+        mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+            @Override
+            public boolean onMarkerClick(Marker marker) {
+
+                //create a method call here which runs the program you want when you click on the marker
+                Toast.makeText(MapsActivity.this, "Thank you for selecting "+marker.getTitle(), Toast.LENGTH_SHORT).show();//debug tool this can be deleted
+
+                return false;
+            }
+        });
+
+
     }
 
     @Override
     public void onRequestFailed(Throwable throwable) {
         throwable.printStackTrace();
+    }
+
+    public void geoLocate(View view) throws IOException {
+
+        EditText et = (EditText) findViewById(R.id.editText);
+        String location = et.getText().toString();
+
+        Geocoder gc = new Geocoder(this);
+        List<android.location.Address> list = gc.getFromLocationName(location,1);
+        android.location.Address address = list.get(0);
+        String locality = address.getLocality();
+
+        Toast.makeText(this, locality, Toast.LENGTH_SHORT).show();
+        float lat = (float) address.getLatitude();
+        float lng = (float) address.getLongitude();
+
+        mMap.addMarker(new MarkerOptions().position(new LatLng(lat, lng)).title(location));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(lat,lng),15));
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("Maps Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
     }
 }
